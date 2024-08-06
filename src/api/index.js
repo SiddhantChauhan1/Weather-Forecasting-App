55185e84e038362f162570ef25de8eca4c5bddca
SiddhantChauhan1 <siddhantchauhan003@gmail.com>
2024-08-06 15:25:35 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const base_URL = 'https://api.weatherapi.com/v1/current.json?key=6a9459dec3224d17bb7193920243107'
+const base_URL = 'https://api.weatherapi.com/v1/current.json?key=#'
 
 export const getWeatherDataForCity = async (city) => {
     const response = await fetch (`${base_URL}&q=${city}&aqi=yes`)
